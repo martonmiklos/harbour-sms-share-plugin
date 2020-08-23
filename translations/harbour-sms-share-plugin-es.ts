f890890bc76d7a77f3a7e33623ffcb0f5b69799a
carlosgonz <45316884+GNUuser@users.noreply.github.com>
2020-08-22 22:27:18 -0400

    Add files via upload

---
+++ translations/harbour-sms-share-plugin-es.ts
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name></name>
+    <message id="harbour-sms-share-plugin-id">
+        <source>SMS</source>
+        <extracomment>Display name for SMS share plugin, just translate as SMS</extracomment>
+        <translation>SMS</translation>
+    </message>
+    <message id="sms-share-ph-recipient">
+        <source>Recipient</source>
+        <extracomment>A single recipient</extracomment>
+        <translation>Destinario</translation>
+    </message>
+    <message id="sms-share-ph-recipients">
+        <source>Recipients</source>
+        <extracomment>Summary of all selected recipients, e.g. &quot;Bob, Jane, 75553243&quot;</extracomment>
+        <translation>Destinarios</translation>
+    </message>
+    <message id="harbour-sms-share-plugin-send">
+        <source>Send</source>
+        <translation>Enviar</translation>
+    </message>
+    <message id="harbour-sms-share-plugin-method">
+        <source>Method</source>
+        <translation>Método</translation>
+    </message>
+    <message id="harbour-sms-share-plugin-short_plaintext">
+        <source>Short plaintext</source>
+        <translation>Texto plano corto</translation>
+    </message>
+    <message id="harbour-sms-share-plugin-full_plaintext">
+        <source>Full plaintext</source>
+        <translation>Texto plano completo</translation>
+    </message>
+    <message id="harbour-sms-share-plugin-smartmessaging_vcard">
+        <source>SmartMessaging vCard</source>
+        <translation>vCard mensajería inteligente</translation>
+    </message>
+    <message id="harbour-sms-share-plugin-new_message">
+        <source>Share contact via SMS</source>
+        <translation>Compartir contacto vía SMS</translation>
+    </message>
+    <message id="harbour-sms-share-plugin-new_message_from_url">
+        <source>Share link via SMS</source>
+        <translation>Compartir enlace vía SMS</translation>
+    </message>
+    <message id="harbour-sms-share-plugin-new_message_from_plaintext">
+        <source>Share text via SMS</source>
+        <translation>Compartir texto vía SMS </translation>
+    </message>
+</context>
+</TS>
