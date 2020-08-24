7b132c4849127ba4191287b6c39786477ed054da
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2020-08-25 03:06:25 +0800

    Create harbour-sms-share-plugin-zh_CN.ts

---
+++ translations/harbour-sms-share-plugin-zh_CN.ts
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name></name>
+    <message id="harbour-sms-share-plugin-id">
+        <source>SMS</source>
+        <extracomment>Display name for SMS share plugin, just translate as SMS</extracomment>
+        <translation>SMS</translation>
+    </message>
+    <message id="sms-share-ph-recipient">
+        <source>Recipient</source>
+        <extracomment>A single recipient</extracomment>
+        <translation>收件人</translation>
+    </message>
+    <message id="sms-share-ph-recipients">
+        <source>Recipients</source>
+        <extracomment>Summary of all selected recipients, e.g. &quot;Bob, Jane, 75553243&quot;</extracomment>
+        <translation>收件人</translation>
+    </message>
+    <message id="harbour-sms-share-plugin-send">
+        <source>Send</source>
+        <translation>发送</translation>
+    </message>
+    <message id="harbour-sms-share-plugin-method">
+        <source>Method</source>
+        <translation>方式</translation>
+    </message>
+    <message id="harbour-sms-share-plugin-short_plaintext">
+        <source>Short plaintext</source>
+        <translation>短纯文本</translation>
+    </message>
+    <message id="harbour-sms-share-plugin-full_plaintext">
+        <source>Full plaintext</source>
+        <translation>完整纯文本</translation>
+    </message>
+    <message id="harbour-sms-share-plugin-smartmessaging_vcard">
+        <source>SmartMessaging vCard</source>
+        <translation>SmartMessaging vCard</translation>
+    </message>
+    <message id="harbour-sms-share-plugin-new_message">
+        <source>Share contact via SMS</source>
+        <translation>通过 SMS 分享联系人</translation>
+    </message>
+    <message id="harbour-sms-share-plugin-new_message_from_url">
+        <source>Share link via SMS</source>
+        <translation>通过 SMS 分享链接</translation>
+    </message>
+    <message id="harbour-sms-share-plugin-new_message_from_plaintext">
+        <source>Share text via SMS</source>
+        <translation>通过 SMS 分享文本</translation>
+    </message>
+</context>
+</TS>
